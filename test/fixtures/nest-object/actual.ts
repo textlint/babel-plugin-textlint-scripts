3832a32e3ec08ac666be1e17ed214342beafd1fc
azu <azuciao@gmail.com>
2020-09-09 22:08:34 +0900

    Add nest tests

---
+++ test/fixtures/nest-object/actual.ts
@@ -0,0 +1,13 @@
+export type Options = {
+    a?: {
+        b: {
+            c?: string;
+        };
+    };
+    d: number;
+    e: 1 | 2 | 3;
+};
+const report = () => {
+    return {};
+};
+export default report;
